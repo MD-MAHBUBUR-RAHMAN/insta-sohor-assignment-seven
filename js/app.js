6de594c5753a75a9f2a23b46a58f5bbfa9748337
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-03-10 22:02:10 +0600

    Description Resolved

--- js/app.js
+++ js/app.js
@@ -27,7 +27,8 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  console.log(text)                     ////======================
+    return text.length < 30 ?'Description: '+ text  :'Description: '+ text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -51,7 +52,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post)
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
